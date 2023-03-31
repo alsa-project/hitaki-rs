alsa-project/hitaki-rs

commit 4838aea84f30176f5bc7a2e3d7b090cf17d76b0d

hitaki/hitaki-sys: regenerate crates depending on glib crate v0.15
The grk-rs project provides gir tool to generate FFI/API crates from
gir files. Current version of the tool generates the crates depending
on glib crate v0.15. On the other hand, existent hitaki crates
depends on glib crate v0.10.

This commit regenerates the crates depending on glib crate v0.15.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

hitaki/src/lib.rs
@@ -1,10 +1,4 @@
 // SPDX-License-Identifier: MIT
-#[macro_use]
-extern crate glib;
-extern crate glib_sys;
-extern crate gobject_sys;
-extern crate hitaki_sys;
-extern crate libc;
 
 mod auto;
 mod efw_protocol;
@@ -27,5 +21,8 @@ pub mod prelude {
 
 /// For subclass implementations derived from provided class.
 pub mod subclass;
+
+// To access to hinawa-sys crate for FFI.
+pub use ffi;
 
 use glib::{object::*, signal::*, translate::*};

hitaki-sys/src/lib.rs
@@ -1,4 +1,5 @@
 // This file was generated by gir (https://github.com/gtk-rs/gir)
+// from
 // from gir-files (https://github.com/gtk-rs/gir-files)
 // DO NOT EDIT
 
@@ -6,17 +7,15 @@
 #![allow(
     clippy::approx_constant,
     clippy::type_complexity,
-    clippy::unreadable_literal
+    clippy::unreadable_literal,
+    clippy::upper_case_acronyms
 )]
-
-extern crate glib_sys as glib;
-extern crate gobject_sys as gobject;
-extern crate libc;
+#![cfg_attr(feature = "dox", feature(doc_cfg))]
 
 #[allow(unused_imports)]
 use libc::{
     c_char, c_double, c_float, c_int, c_long, c_short, c_uchar, c_uint, c_ulong, c_ushort, c_void,
-    intptr_t, size_t, ssize_t, time_t, uintptr_t, FILE,
+    intptr_t, size_t, ssize_t, uintptr_t, FILE,
 };
 
 #[allow(unused_imports)]
@@ -64,8 +63,8 @@ pub const HITAKI_EFW_PROTOCOL_ERROR_INCOMPLETE: HitakiEfwProtocolError = -214748
 pub const HITAKI_EFW_PROTOCOL_ERROR_INVALID: HitakiEfwProtocolError = -1;
 
 // Records
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiAlsaFirewireInterface {
     pub iface: gobject::GTypeInterface,
     pub open: Option<
@@ -91,21 +90,18 @@ pub struct HitakiAlsaFirewireInterface {
 
 impl ::std::fmt::Debug for HitakiAlsaFirewireInterface {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!(
-            "HitakiAlsaFirewireInterface @ {:?}",
-            self as *const _
-        ))
-        .field("iface", &self.iface)
-        .field("open", &self.open)
-        .field("lock", &self.lock)
-        .field("unlock", &self.unlock)
-        .field("create_source", &self.create_source)
-        .finish()
+        f.debug_struct(&format!("HitakiAlsaFirewireInterface @ {:p}", self))
+            .field("iface", &self.iface)
+            .field("open", &self.open)
+            .field("lock", &self.lock)
+            .field("unlock", &self.unlock)
+            .field("create_source", &self.create_source)
+            .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiEfwProtocolInterface {
     pub iface: gobject::GTypeInterface,
     pub transmit_request: Option<
@@ -133,20 +129,17 @@ pub struct HitakiEfwProtocolInterface {
 
 impl ::std::fmt::Debug for HitakiEfwProtocolInterface {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!(
-            "HitakiEfwProtocolInterface @ {:?}",
-            self as *const _
-        ))
-        .field("iface", &self.iface)
-        .field("transmit_request", &self.transmit_request)
-        .field("get_seqnum", &self.get_seqnum)
-        .field("responded", &self.responded)
-        .finish()
+        f.debug_struct(&format!("HitakiEfwProtocolInterface @ {:p}", self))
+            .field("iface", &self.iface)
+            .field("transmit_request", &self.transmit_request)
+            .field("get_seqnum", &self.get_seqnum)
+            .field("responded", &self.responded)
+            .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiMotuCommandDspInterface {
     pub iface: gobject::GTypeInterface,
     pub read_float_meter: Option<
@@ -160,18 +153,15 @@ pub struct HitakiMotuCommandDspInterface {
 
 impl ::std::fmt::Debug for HitakiMotuCommandDspInterface {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!(
-            "HitakiMotuCommandDspInterface @ {:?}",
-            self as *const _
-        ))
-        .field("iface", &self.iface)
-        .field("read_float_meter", &self.read_float_meter)
-        .finish()
+        f.debug_struct(&format!("HitakiMotuCommandDspInterface @ {:p}", self))
+            .field("iface", &self.iface)
+            .field("read_float_meter", &self.read_float_meter)
+            .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiMotuRegisterDspInterface {
     pub iface: gobject::GTypeInterface,
     pub read_parameter: Option<
@@ -193,20 +183,17 @@ pub struct HitakiMotuRegisterDspInterface {
 
 impl ::std::fmt::Debug for HitakiMotuRegisterDspInterface {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!(
-            "HitakiMotuRegisterDspInterface @ {:?}",
-            self as *const _
-        ))
-        .field("iface", &self.iface)
-        .field("read_parameter", &self.read_parameter)
-        .field("read_byte_meter", &self.read_byte_meter)
-        .field("changed", &self.changed)
-        .finish()
+        f.debug_struct(&format!("HitakiMotuRegisterDspInterface @ {:p}", self))
+            .field("iface", &self.iface)
+            .field("read_parameter", &self.read_parameter)
+            .field("read_byte_meter", &self.read_byte_meter)
+            .field("changed", &self.changed)
+            .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiQuadletNotificationInterface {
     pub iface: gobject::GTypeInterface,
     pub notified: Option<unsafe extern "C" fn(*mut HitakiQuadletNotification, u32)>,
@@ -214,118 +201,112 @@ pub struct HitakiQuadletNotificationInterface {
 
 impl ::std::fmt::Debug for HitakiQuadletNotificationInterface {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!(
-            "HitakiQuadletNotificationInterface @ {:?}",
-            self as *const _
-        ))
-        .field("iface", &self.iface)
-        .field("notified", &self.notified)
-        .finish()
+        f.debug_struct(&format!("HitakiQuadletNotificationInterface @ {:p}", self))
+            .field("iface", &self.iface)
+            .field("notified", &self.notified)
+            .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndDiceClass {
     pub parent_class: gobject::GObjectClass,
 }
 
 impl ::std::fmt::Debug for HitakiSndDiceClass {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndDiceClass @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndDiceClass @ {:p}", self))
             .field("parent_class", &self.parent_class)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndDigi00xClass {
     pub parent_class: gobject::GObjectClass,
 }
 
 impl ::std::fmt::Debug for HitakiSndDigi00xClass {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndDigi00xClass @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndDigi00xClass @ {:p}", self))
             .field("parent_class", &self.parent_class)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndEfwClass {
     pub parent_class: gobject::GObjectClass,
 }
 
 impl ::std::fmt::Debug for HitakiSndEfwClass {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndEfwClass @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndEfwClass @ {:p}", self))
             .field("parent_class", &self.parent_class)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndMotuClass {
     pub parent_class: gobject::GObjectClass,
 }
 
 impl ::std::fmt::Debug for HitakiSndMotuClass {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndMotuClass @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndMotuClass @ {:p}", self))
             .field("parent_class", &self.parent_class)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndMotuRegisterDspParameter {
     pub reserved: [u8; 512],
 }
 
 impl ::std::fmt::Debug for HitakiSndMotuRegisterDspParameter {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!(
-            "HitakiSndMotuRegisterDspParameter @ {:?}",
-            self as *const _
-        ))
-        .finish()
+        f.debug_struct(&format!("HitakiSndMotuRegisterDspParameter @ {:p}", self))
+            .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndTascamClass {
     pub parent_class: gobject::GObjectClass,
 }
 
 impl ::std::fmt::Debug for HitakiSndTascamClass {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndTascamClass @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndTascamClass @ {:p}", self))
             .field("parent_class", &self.parent_class)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndUnitClass {
     pub parent_class: gobject::GObjectClass,
 }
 
 impl ::std::fmt::Debug for HitakiSndUnitClass {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndUnitClass @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndUnitClass @ {:p}", self))
             .field("parent_class", &self.parent_class)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiTascamProtocolInterface {
     pub iface: gobject::GTypeInterface,
     pub read_state: Option<
@@ -341,97 +322,94 @@ pub struct HitakiTascamProtocolInterface {
 
 impl ::std::fmt::Debug for HitakiTascamProtocolInterface {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!(
-            "HitakiTascamProtocolInterface @ {:?}",
-            self as *const _
-        ))
-        .field("iface", &self.iface)
-        .field("read_state", &self.read_state)
-        .field("changed", &self.changed)
-        .finish()
+        f.debug_struct(&format!("HitakiTascamProtocolInterface @ {:p}", self))
+            .field("iface", &self.iface)
+            .field("read_state", &self.read_state)
+            .field("changed", &self.changed)
+            .finish()
     }
 }
 
 // Classes
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndDice {
     pub parent_instance: gobject::GObject,
 }
 
 impl ::std::fmt::Debug for HitakiSndDice {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndDice @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndDice @ {:p}", self))
             .field("parent_instance", &self.parent_instance)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndDigi00x {
     pub parent_instance: gobject::GObject,
 }
 
 impl ::std::fmt::Debug for HitakiSndDigi00x {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndDigi00x @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndDigi00x @ {:p}", self))
             .field("parent_instance", &self.parent_instance)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndEfw {
     pub parent_instance: gobject::GObject,
 }
 
 impl ::std::fmt::Debug for HitakiSndEfw {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndEfw @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndEfw @ {:p}", self))
             .field("parent_instance", &self.parent_instance)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndMotu {
     pub parent_instance: gobject::GObject,
 }
 
 impl ::std::fmt::Debug for HitakiSndMotu {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndMotu @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndMotu @ {:p}", self))
             .field("parent_instance", &self.parent_instance)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndTascam {
     pub parent_instance: gobject::GObject,
 }
 
 impl ::std::fmt::Debug for HitakiSndTascam {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndTascam @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndTascam @ {:p}", self))
             .field("parent_instance", &self.parent_instance)
             .finish()
     }
 }
 
-#[repr(C)]
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct HitakiSndUnit {
     pub parent_instance: gobject::GObject,
 }
 
 impl ::std::fmt::Debug for HitakiSndUnit {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.debug_struct(&format!("HitakiSndUnit @ {:?}", self as *const _))
+        f.debug_struct(&format!("HitakiSndUnit @ {:p}", self))
             .field("parent_instance", &self.parent_instance)
             .finish()
     }
@@ -439,59 +417,78 @@ impl ::std::fmt::Debug for HitakiSndUnit {
 
 // Interfaces
 #[repr(C)]
-pub struct HitakiAlsaFirewire(c_void);
+pub struct HitakiAlsaFirewire {
+    _data: [u8; 0],
+    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 impl ::std::fmt::Debug for HitakiAlsaFirewire {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        write!(f, "HitakiAlsaFirewire @ {:?}", self as *const _)
+        write!(f, "HitakiAlsaFirewire @ {:p}", self)
     }
 }
 
 #[repr(C)]
-pub struct HitakiEfwProtocol(c_void);
+pub struct HitakiEfwProtocol {
+    _data: [u8; 0],
+    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 impl ::std::fmt::Debug for HitakiEfwProtocol {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        write!(f, "HitakiEfwProtocol @ {:?}", self as *const _)
+        write!(f, "HitakiEfwProtocol @ {:p}", self)
     }
 }
 
 #[repr(C)]
-pub struct HitakiMotuCommandDsp(c_void);
+pub struct HitakiMotuCommandDsp {
+    _data: [u8; 0],
+    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 impl ::std::fmt::Debug for HitakiMotuCommandDsp {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        write!(f, "HitakiMotuCommandDsp @ {:?}", self as *const _)
+        write!(f, "HitakiMotuCommandDsp @ {:p}", self)
     }
 }
 
 #[repr(C)]
-pub struct HitakiMotuRegisterDsp(c_void);
+pub struct HitakiMotuRegisterDsp {
+    _data: [u8; 0],
+    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 impl ::std::fmt::Debug for HitakiMotuRegisterDsp {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        write!(f, "HitakiMotuRegisterDsp @ {:?}", self as *const _)
+        write!(f, "HitakiMotuRegisterDsp @ {:p}", self)
     }
 }
 
 #[repr(C)]
-pub struct HitakiQuadletNotification(c_void);
+pub struct HitakiQuadletNotification {
+    _data: [u8; 0],
+    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 impl ::std::fmt::Debug for HitakiQuadletNotification {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        write!(f, "HitakiQuadletNotification @ {:?}", self as *const _)
+        write!(f, "HitakiQuadletNotification @ {:p}", self)
     }
 }
 
 #[repr(C)]
-pub struct HitakiTascamProtocol(c_void);
+pub struct HitakiTascamProtocol {
+    _data: [u8; 0],
+    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 impl ::std::fmt::Debug for HitakiTascamProtocol {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        write!(f, "HitakiTascamProtocol @ {:?}", self as *const _)
+        write!(f, "HitakiTascamProtocol @ {:p}", self)
     }
 }
 
+#[link(name = "hitaki")]
 extern "C" {
 
     //=========================================================================

hitaki-sys/Cargo.toml
@@ -2,6 +2,7 @@
 authors = ["Takashi Sakamoto <o-takashi@sakamocchi.jp>"]
 build = "build.rs"
 description = "FFI bindings for libhitaki library"
+edition = "2018"
 license = "MIT"
 links = "hitaki"
 name = "hitaki-sys"
@@ -13,28 +14,26 @@ features = ["dox"]
 name = "hitaki"
 version = "0.1"
 
-[package.metadata.system-deps.hitaki.feature-versions]
-
 [lib]
 name = "hitaki_sys"
 
 [build-dependencies]
 pkg-config = "0.3.7"
-system-deps = "1.3"
+system-deps = "6"
 
 [dependencies]
 libc = "0.2"
 
-[dependencies.glib_sys]
+[dependencies.glib]
 package = "glib-sys"
-version = "0.10"
+version = "0.15"
 
-[dependencies.gobject_sys]
+[dependencies.gobject]
 package = "gobject-sys"
-version = "0.10"
+version = "0.15"
 
 [dev-dependencies]
-shell-words = "0.1.0"
+shell-words = "1.0.0"
 tempfile = "3"
 
 [features]

hitaki-sys/Gir.toml
@@ -1,4 +1,5 @@
 [options]
+girs_directories = ["../gir-files", ".."]
 work_mode = "sys"
 library = "Hitaki"
 version = "0.0"

hitaki-sys/build.rs
@@ -1,9 +1,7 @@
 // This file was generated by gir (https://github.com/gtk-rs/gir)
+// from
 // from gir-files (https://github.com/gtk-rs/gir-files)
 // DO NOT EDIT
-
-#[cfg(not(feature = "dox"))]
-extern crate system_deps;
 
 #[cfg(not(feature = "dox"))]
 use std::process;
@@ -14,7 +12,7 @@ fn main() {} // prevent linking libraries to avoid documentation failure
 #[cfg(not(feature = "dox"))]
 fn main() {
     if let Err(s) = system_deps::Config::new().probe() {
-        let _ = eprintln!("{}", s);
+        println!("cargo:warning={}", s);
         process::exit(1);
     }
 }

hitaki-sys/tests/abi.rs
@@ -1,13 +1,12 @@
 // This file was generated by gir (https://github.com/gtk-rs/gir)
+// from
 // from gir-files (https://github.com/gtk-rs/gir-files)
 // DO NOT EDIT
 
-extern crate hitaki_sys;
-extern crate shell_words;
-extern crate tempfile;
 use hitaki_sys::*;
 use std::env;
 use std::error::Error;
+use std::ffi::OsString;
 use std::mem::{align_of, size_of};
 use std::path::Path;
 use std::process::Command;
@@ -22,23 +21,17 @@ struct Compiler {
 }
 
 impl Compiler {
-    pub fn new() -> Result<Compiler, Box<dyn Error>> {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
         let mut args = get_var("CC", "cc")?;
         args.push("-Wno-deprecated-declarations".to_owned());
+        // For _Generic
+        args.push("-std=c11".to_owned());
         // For %z support in printf when using MinGW.
         args.push("-D__USE_MINGW_ANSI_STDIO".to_owned());
         args.extend(get_var("CFLAGS", "")?);
         args.extend(get_var("CPPFLAGS", "")?);
         args.extend(pkg_config_cflags(PACKAGES)?);
-        Ok(Compiler { args })
-    }
-
-    pub fn define<'a, V: Into<Option<&'a str>>>(&mut self, var: &str, val: V) {
-        let arg = match val.into() {
-            None => format!("-D{}", var),
-            Some(val) => format!("-D{}={}", var, val),
-        };
-        self.args.push(arg);
+        Ok(Self { args })
     }
 
     pub fn compile(&self, src: &Path, out: &Path) -> Result<(), Box<dyn Error>> {
@@ -72,7 +65,8 @@ fn pkg_config_cflags(packages: &[&str]) -> Result<Vec<String>, Box<dyn Error>> {
     if packages.is_empty() {
         return Ok(Vec::new());
     }
-    let mut cmd = Command::new("pkg-config");
+    let pkg_config = env::var_os("PKG_CONFIG").unwrap_or_else(|| OsString::from("pkg-config"));
+    let mut cmd = Command::new(pkg_config);
     cmd.arg("--cflags");
     cmd.args(packages);
     let out = cmd.output()?;
@@ -95,8 +89,6 @@ struct Results {
     passed: usize,
     /// Total number of failed tests (including those that failed to compile).
     failed: usize,
-    /// Number of tests that failed to compile.
-    failed_to_compile: usize,
 }
 
 impl Results {
@@ -106,15 +98,8 @@ impl Results {
     fn record_failed(&mut self) {
         self.failed += 1;
     }
-    fn record_failed_to_compile(&mut self) {
-        self.failed += 1;
-        self.failed_to_compile += 1;
-    }
     fn summary(&self) -> String {
-        format!(
-            "{} passed; {} failed (compilation errors: {})",
-            self.passed, self.failed, self.failed_to_compile
-        )
+        format!("{} passed; {} failed", self.passed, self.failed)
     }
     fn expect_total_success(&self) {
         if self.failed == 0 {
@@ -126,93 +111,97 @@ impl Results {
 }
 
 #[test]
+#[cfg(target_os = "linux")]
 fn cross_validate_constants_with_c() {
-    let tmpdir = Builder::new()
-        .prefix("abi")
-        .tempdir()
-        .expect("temporary directory");
-    let cc = Compiler::new().expect("configured compiler");
+    let mut c_constants: Vec<(String, String)> = Vec::new();
 
-    assert_eq!(
-        "1",
-        get_c_value(tmpdir.path(), &cc, "1").expect("C constant"),
-        "failed to obtain correct constant value for 1"
-    );
-
-    let mut results: Results = Default::default();
-    for (i, &(name, rust_value)) in RUST_CONSTANTS.iter().enumerate() {
-        match get_c_value(tmpdir.path(), &cc, name) {
-            Err(e) => {
-                results.record_failed_to_compile();
-                eprintln!("{}", e);
-            }
-            Ok(ref c_value) => {
-                if rust_value == c_value {
-                    results.record_passed();
-                } else {
-                    results.record_failed();
-                    eprintln!(
-                        "Constant value mismatch for {}\nRust: {:?}\nC:    {:?}",
-                        name, rust_value, c_value
-                    );
-                }
-            }
-        };
-        if (i + 1) % 25 == 0 {
-            println!("constants ... {}", results.summary());
-        }
+    for l in get_c_output("constant").unwrap().lines() {
+        let mut words = l.trim().split(';');
+        let name = words.next().expect("Failed to parse name").to_owned();
+        let value = words
+            .next()
+            .and_then(|s| s.parse().ok())
+            .expect("Failed to parse value");
+        c_constants.push((name, value));
     }
+
+    let mut results = Results::default();
+
+    for ((rust_name, rust_value), (c_name, c_value)) in
+        RUST_CONSTANTS.iter().zip(c_constants.iter())
+    {
+        if rust_name != c_name {
+            results.record_failed();
+            eprintln!("Name mismatch:\nRust: {:?}\nC:    {:?}", rust_name, c_name,);
+            continue;
+        }
+
+        if rust_value != c_value {
+            results.record_failed();
+            eprintln!(
+                "Constant value mismatch for {}\nRust: {:?}\nC:    {:?}",
+                rust_name, rust_value, &c_value
+            );
+            continue;
+        }
+
+        results.record_passed();
+    }
+
     results.expect_total_success();
 }
 
 #[test]
+#[cfg(target_os = "linux")]
 fn cross_validate_layout_with_c() {
-    let tmpdir = Builder::new()
-        .prefix("abi")
-        .tempdir()
-        .expect("temporary directory");
-    let cc = Compiler::new().expect("configured compiler");
+    let mut c_layouts = Vec::new();
 
-    assert_eq!(
-        Layout {
-            size: 1,
-            alignment: 1
-        },
-        get_c_layout(tmpdir.path(), &cc, "char").expect("C layout"),
-        "failed to obtain correct layout for char type"
-    );
-
-    let mut results: Results = Default::default();
-    for (i, &(name, rust_layout)) in RUST_LAYOUTS.iter().enumerate() {
-        match get_c_layout(tmpdir.path(), &cc, name) {
-            Err(e) => {
-                results.record_failed_to_compile();
-                eprintln!("{}", e);
-            }
-            Ok(c_layout) => {
-                if rust_layout == c_layout {
-                    results.record_passed();
-                } else {
-                    results.record_failed();
-                    eprintln!(
-                        "Layout mismatch for {}\nRust: {:?}\nC:    {:?}",
-                        name, rust_layout, &c_layout
-                    );
-                }
-            }
-        };
-        if (i + 1) % 25 == 0 {
-            println!("layout    ... {}", results.summary());
-        }
+    for l in get_c_output("layout").unwrap().lines() {
+        let mut words = l.trim().split(';');
+        let name = words.next().expect("Failed to parse name").to_owned();
+        let size = words
+            .next()
+            .and_then(|s| s.parse().ok())
+            .expect("Failed to parse size");
+        let alignment = words
+            .next()
+            .and_then(|s| s.parse().ok())
+            .expect("Failed to parse alignment");
+        c_layouts.push((name, Layout { size, alignment }));
     }
+
+    let mut results = Results::default();
+
+    for ((rust_name, rust_layout), (c_name, c_layout)) in RUST_LAYOUTS.iter().zip(c_layouts.iter())
+    {
+        if rust_name != c_name {
+            results.record_failed();
+            eprintln!("Name mismatch:\nRust: {:?}\nC:    {:?}", rust_name, c_name,);
+            continue;
+        }
+
+        if rust_layout != c_layout {
+            results.record_failed();
+            eprintln!(
+                "Layout mismatch for {}\nRust: {:?}\nC:    {:?}",
+                rust_name, rust_layout, &c_layout
+            );
+            continue;
+        }
+
+        results.record_passed();
+    }
+
     results.expect_total_success();
 }
 
-fn get_c_layout(dir: &Path, cc: &Compiler, name: &str) -> Result<Layout, Box<dyn Error>> {
-    let exe = dir.join("layout");
-    let mut cc = cc.clone();
-    cc.define("ABI_TYPE_NAME", name);
-    cc.compile(Path::new("tests/layout.c"), &exe)?;
+fn get_c_output(name: &str) -> Result<String, Box<dyn Error>> {
+    let tmpdir = Builder::new().prefix("abi").tempdir()?;
+    let exe = tmpdir.path().join(name);
+    let c_file = Path::new("tests").join(name).with_extension("c");
+
+    let cc = Compiler::new().expect("configured compiler");
+    cc.compile(&c_file, &exe)?;
 
     let mut abi_cmd = Command::new(exe);
     let output = abi_cmd.output()?;
@@ -220,35 +209,7 @@ fn get_c_layout(dir: &Path, cc: &Compiler, name: &str) -> Result<Layout, Box<dyn
         return Err(format!("command {:?} failed, {:?}", &abi_cmd, &output).into());
     }
 
-    let stdout = str::from_utf8(&output.stdout)?;
-    let mut words = stdout.trim().split_whitespace();
-    let size = words.next().unwrap().parse().unwrap();
-    let alignment = words.next().unwrap().parse().unwrap();
-    Ok(Layout { size, alignment })
-}
-
-fn get_c_value(dir: &Path, cc: &Compiler, name: &str) -> Result<String, Box<dyn Error>> {
-    let exe = dir.join("constant");
-    let mut cc = cc.clone();
-    cc.define("ABI_CONSTANT_NAME", name);
-    cc.compile(Path::new("tests/constant.c"), &exe)?;
-
-    let mut abi_cmd = Command::new(exe);
-    let output = abi_cmd.output()?;
-    if !output.status.success() {
-        return Err(format!("command {:?} failed, {:?}", &abi_cmd, &output).into());
-    }
-
-    let output = str::from_utf8(&output.stdout)?.trim();
-    if !output.starts_with("###gir test###") || !output.ends_with("###gir test###") {
-        return Err(format!(
-            "command {:?} return invalid output, {:?}",
-            &abi_cmd, &output
-        )
-        .into());
-    }
-
-    Ok(String::from(&output[14..(output.len() - 14)]))
+    Ok(String::from_utf8(output.stdout)?)
 }
 
 const RUST_LAYOUTS: &[(&str, Layout)] = &[

commit efa30ae26681d2a5d4d7a5a69cb40bcc3b393def

hitaki: fix copy-and-paste mistake
Fixes: 4838aea84f30 ("hitaki/hitaki-sys: regenerate crates depending on glib crate v0.15")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

hitaki/src/lib.rs
@@ -24,7 +24,7 @@ pub mod prelude {
 /// For subclass implementations derived from provided class.
 pub mod subclass;
 
-// To access to hinawa-sys crate for FFI.
+// To access to hitaki-sys crate for FFI.
 pub use ffi;
 
 // For links in documentation.