alsa-project/hitaki-rs

commit db5e859ea7da98e22102204e6e54b23a8b2d49fe

hitaki: regenerate crate for libhitaki v0.3.0 release
This commit regenerates API crate for the new release.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

hitaki/src/subclass/timestamped_quadlet_notification.rs
@@ -1,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+use super::*;
+
+/// Trait which should be implemented by subclass of
+/// [`TimestampedQuadletNotification`][crate::TimestampedQuadletNotification].
+pub trait TimestampedQuadletNotificationImpl: ObjectImpl {
+    fn notified_at(&self, unit: &Self::Type, msg: u32, tstamp: u32);
+}
+
+/// Trait which is automatically implemented to implementator of
+/// [`TimestampedQuadletNotificationImpl`][self::TimestampedQuadletNotificationImpl]
+pub trait TimestampedQuadletNotificationImplExt: ObjectSubclass {
+    fn parent_notified_at(&self, unit: &Self::Type, msg: u32, tstamp: u32);
+}
+
+impl<T: TimestampedQuadletNotificationImpl> TimestampedQuadletNotificationImplExt for T {
+    fn parent_notified_at(&self, unit: &Self::Type, msg: u32, tstamp: u32) {
+        unsafe {
+            let type_data = Self::type_data();
+            let parent_iface = type_data
+                .as_ref()
+                .parent_interface::<TimestampedQuadletNotification>()
+                as *const ffi::HitakiTimestampedQuadletNotificationInterface;
+            let func = (*parent_iface)
+                .notified_at
+                .expect("no parent \"notified_at\" implementation");
+            func(
+                unit.unsafe_cast_ref::<TimestampedQuadletNotification>()
+                    .to_glib_none()
+                    .0,
+                msg.into(),
+                tstamp.into(),
+            )
+        }
+    }
+}
+
+unsafe impl<T: TimestampedQuadletNotificationImpl> IsImplementable<T>
+    for TimestampedQuadletNotification
+{
+    fn interface_init(iface: &mut Interface<Self>) {
+        let iface = iface.as_mut();
+        iface.notified_at = Some(timestamped_quadlet_notification_notified_at::<T>);
+    }
+}
+
+unsafe extern "C" fn timestamped_quadlet_notification_notified_at<
+    T: TimestampedQuadletNotificationImpl,
+>(
+    unit: *mut ffi::HitakiTimestampedQuadletNotification,
+    msg: c_uint,
+    tstamp: c_uint,
+) {
+    let instance = &*(unit as *mut T::Instance);
+    let imp = instance.imp();
+    imp.notified_at(
+        from_glib_borrow::<_, TimestampedQuadletNotification>(unit).unsafe_cast_ref(),
+        msg.into(),
+        tstamp.into(),
+    )
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{prelude::*, subclass::timestamped_quadlet_notification::*};
+    use glib::{
+        subclass::{object::*, types::*},
+        Object, ParamFlags, ParamSpec, ParamSpecUInt, ToValue, Value,
+    };
+
+    mod imp {
+        use super::*;
+        use std::cell::RefCell;
+
+        #[derive(Default)]
+        pub struct TimestampedQuadletNotificationTest(RefCell<(u32, u32)>);
+
+        #[glib::object_subclass]
+        impl ObjectSubclass for TimestampedQuadletNotificationTest {
+            const NAME: &'static str = "TimestampedQuadletNotificationTest";
+            type Type = super::TimestampedQuadletNotificationTest;
+            type Interfaces = (TimestampedQuadletNotification,);
+
+            fn new() -> Self {
+                Self::default()
+            }
+        }
+
+        impl ObjectImpl for TimestampedQuadletNotificationTest {
+            fn properties() -> &'static [ParamSpec] {
+                use once_cell::sync::Lazy;
+                static PROPERTIES: Lazy<Vec<ParamSpec>> = Lazy::new(|| {
+                    vec![ParamSpecUInt::new(
+                        "msg",
+                        "msg",
+                        "the message in notification",
+                        0,
+                        0x0000ffff as u32,
+                        0,
+                        ParamFlags::READABLE,
+                    ),
+                    ParamSpecUInt::new(
+                        "tstamp",
+                        "tstamp",
+                        "the time stamp in notification",
+                        0,
+                        0x0000ffff as u32,
+                        0,
+                        ParamFlags::READABLE,
+                    )]
+                });
+
+                PROPERTIES.as_ref()
+            }
+
+            fn property(&self, _obj: &Self::Type, _id: usize, pspec: &ParamSpec) -> Value {
+                match pspec.name() {
+                    "msg" => self.0.borrow().0.to_value(),
+                    "tstamp" => self.0.borrow().1.to_value(),
+                    _ => unimplemented!(),
+                }
+            }
+        }
+
+        impl TimestampedQuadletNotificationImpl for TimestampedQuadletNotificationTest {
+            fn notified_at(&self, _unit: &Self::Type, msg: u32, tstamp: u32) {
+                *self.0.borrow_mut() = (msg, tstamp);
+            }
+        }
+    }
+
+    glib::wrapper! {
+        pub struct TimestampedQuadletNotificationTest(ObjectSubclass<imp::TimestampedQuadletNotificationTest>)
+            @implements TimestampedQuadletNotification;
+    }
+
+    #[allow(clippy::new_without_default)]
+    impl TimestampedQuadletNotificationTest {
+        pub fn new() -> Self {
+            Object::new(&[]).expect(
+                "Failed creation/initialization of TimestampedQuadletNotificationTest object",
+            )
+        }
+
+        pub fn msg(&self) -> u32 {
+            self.property::<u32>("msg")
+        }
+
+        pub fn tstamp(&self) -> u32 {
+            self.property::<u32>("tstamp")
+        }
+    }
+
+    #[test]
+    fn quadlet_notification_iface() {
+        let unit = TimestampedQuadletNotificationTest::new();
+
+        assert_eq!(unit.msg(), 0);
+        unit.emit_notified_at(123, 456);
+        assert_eq!(unit.msg(), 123);
+        assert_eq!(unit.tstamp(), 456);
+    }
+}

hitaki/src/auto/timestamped_quadlet_notification.rs
@@ -1,0 +1,100 @@
+// This file was generated by gir (https://github.com/gtk-rs/gir)
+// from
+// from gir-files (https://github.com/gtk-rs/gir-files)
+// DO NOT EDIT
+
+use glib::object::Cast;
+use glib::object::IsA;
+use glib::object::ObjectExt;
+use glib::signal::connect_raw;
+use glib::signal::SignalHandlerId;
+use glib::translate::*;
+use std::boxed::Box as Box_;
+use std::fmt;
+use std::mem::transmute;
+
+glib::wrapper! {
+    /// An interface to operate notification with quadlet message and time stamp.
+    ///
+    /// Some of units supported by drivers in ALSA firewire stack have the function to notify quadlet
+    /// message and time stamp for specific purposes. The [`TimestampedQuadletNotification`][crate::TimestampedQuadletNotification] is an
+    /// interface to operate the notification.
+    ///
+    /// # Implements
+    ///
+    /// [`TimestampedQuadletNotificationExt`][trait@crate::prelude::TimestampedQuadletNotificationExt]
+    #[doc(alias = "HitakiTimestampedQuadletNotification")]
+    pub struct TimestampedQuadletNotification(Interface<ffi::HitakiTimestampedQuadletNotification, ffi::HitakiTimestampedQuadletNotificationInterface>);
+
+    match fn {
+        type_ => || ffi::hitaki_timestamped_quadlet_notification_get_type(),
+    }
+}
+
+impl TimestampedQuadletNotification {
+    pub const NONE: Option<&'static TimestampedQuadletNotification> = None;
+}
+
+/// Trait containing all [`struct@TimestampedQuadletNotification`] methods.
+///
+/// # Implementors
+///
+/// [`SndFireface`][struct@crate::SndFireface], [`TimestampedQuadletNotification`][struct@crate::TimestampedQuadletNotification]
+pub trait TimestampedQuadletNotificationExt: 'static {
+    /// Emitted when the target unit transfers notification.
+    ///
+    /// The value of @tstamp is unsigned 16 bit integer including higher 3 bits for three low order
+    /// bits of second field and the rest 13 bits for cycle field in the format of IEEE 1394
+    /// CYCLE_TIMER register.
+    /// ## `message`
+    /// A quadlet message in notification.
+    /// ## `tstamp`
+    /// The isochronous cycle at which the request arrived.
+    /// Emitted when the target unit transfers notification.
+    #[doc(alias = "notified-at")]
+    fn connect_notified_at<F: Fn(&Self, u32, u32) + 'static>(&self, f: F) -> SignalHandlerId;
+
+    fn emit_notified_at(&self, message: u32, tstamp: u32);
+}
+
+impl<O: IsA<TimestampedQuadletNotification>> TimestampedQuadletNotificationExt for O {
+    fn connect_notified_at<F: Fn(&Self, u32, u32) + 'static>(&self, f: F) -> SignalHandlerId {
+        unsafe extern "C" fn notified_at_trampoline<
+            P: IsA<TimestampedQuadletNotification>,
+            F: Fn(&P, u32, u32) + 'static,
+        >(
+            this: *mut ffi::HitakiTimestampedQuadletNotification,
+            message: libc::c_uint,
+            tstamp: libc::c_uint,
+            f: glib::ffi::gpointer,
+        ) {
+            let f: &F = &*(f as *const F);
+            f(
+                TimestampedQuadletNotification::from_glib_borrow(this).unsafe_cast_ref(),
+                message,
+                tstamp,
+            )
+        }
+        unsafe {
+            let f: Box_<F> = Box_::new(f);
+            connect_raw(
+                self.as_ptr() as *mut _,
+                b"notified-at\0".as_ptr() as *const _,
+                Some(transmute::<_, unsafe extern "C" fn()>(
+                    notified_at_trampoline::<Self, F> as *const (),
+                )),
+                Box_::into_raw(f),
+            )
+        }
+    }
+
+    fn emit_notified_at(&self, message: u32, tstamp: u32) {
+        self.emit_by_name::<()>("notified-at", &[&message, &tstamp]);
+    }
+}
+
+impl fmt::Display for TimestampedQuadletNotification {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("TimestampedQuadletNotification")
+    }
+}

hitaki/Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 edition = "2018"
 name = "hitaki"
-version = "0.2.0"
+version = "0.3.0"
 # For publishing.
 authors = ["Takashi Sakamoto"]
 categories = ["api-bindings", "hardware-support", "os::linux-apis"]

hitaki/Gir.toml
@@ -11,10 +11,12 @@ generate = [
     "Hitaki.AlsaFirewireType",
     "Hitaki.AlsaFirewire",
     "Hitaki.QuadletNotification",
+    "Hitaki.TimestampedQuadletNotification",
     "Hitaki.SndUnit",
     "Hitaki.SndDice",
     "Hitaki.SndDigi00x",
     "Hitaki.SndEfw",
+    "Hitaki.SndFireface",
     "Hitaki.SndMotu",
     "Hitaki.SndTascam",
 ]

hitaki/src/auto/alsa_firewire.rs
@@ -41,7 +41,7 @@ impl AlsaFirewire {
 ///
 /// # Implementors
 ///
-/// [`AlsaFirewire`][struct@crate::AlsaFirewire], [`SndDice`][struct@crate::SndDice], [`SndDigi00x`][struct@crate::SndDigi00x], [`SndEfw`][struct@crate::SndEfw], [`SndMotu`][struct@crate::SndMotu], [`SndTascam`][struct@crate::SndTascam], [`SndUnit`][struct@crate::SndUnit]
+/// [`AlsaFirewire`][struct@crate::AlsaFirewire], [`SndDice`][struct@crate::SndDice], [`SndDigi00x`][struct@crate::SndDigi00x], [`SndEfw`][struct@crate::SndEfw], [`SndFireface`][struct@crate::SndFireface], [`SndMotu`][struct@crate::SndMotu], [`SndTascam`][struct@crate::SndTascam], [`SndUnit`][struct@crate::SndUnit]
 pub trait AlsaFirewireExt: 'static {
     /// Allocate [`glib::Source`][crate::glib::Source]  to handle events from ALSA HwDep character device.
     ///

hitaki/src/auto/efw_protocol.rs
@@ -31,7 +31,7 @@ impl EfwProtocol {
     pub const NONE: Option<&'static EfwProtocol> = None;
 }
 
-/// Trait containing the part of[`struct@EfwProtocol`] methods.
+/// Trait containing the part of [`struct@EfwProtocol`] methods.
 ///
 /// # Implementors
 ///

hitaki/src/auto/mod.rs
@@ -27,6 +27,9 @@ pub use self::snd_digi00x::SndDigi00x;
 mod snd_efw;
 pub use self::snd_efw::SndEfw;
 
+mod snd_fireface;
+pub use self::snd_fireface::SndFireface;
+
 mod snd_motu;
 pub use self::snd_motu::SndMotu;
 
@@ -38,6 +41,9 @@ pub use self::snd_unit::SndUnit;
 
 mod tascam_protocol;
 pub use self::tascam_protocol::TascamProtocol;
+
+mod timestamped_quadlet_notification;
+pub use self::timestamped_quadlet_notification::TimestampedQuadletNotification;
 
 mod snd_motu_register_dsp_parameter;
 pub use self::snd_motu_register_dsp_parameter::SndMotuRegisterDspParameter;
@@ -55,4 +61,5 @@ pub mod traits {
     pub use super::motu_register_dsp::MotuRegisterDspExt;
     pub use super::quadlet_notification::QuadletNotificationExt;
     pub use super::tascam_protocol::TascamProtocolExt;
+    pub use super::timestamped_quadlet_notification::TimestampedQuadletNotificationExt;
 }

hitaki/src/auto/snd_fireface.rs
@@ -1,0 +1,52 @@
+// This file was generated by gir (https://github.com/gtk-rs/gir)
+// from
+// from gir-files (https://github.com/gtk-rs/gir-files)
+// DO NOT EDIT
+
+use crate::AlsaFirewire;
+use crate::TimestampedQuadletNotification;
+use glib::translate::*;
+use std::fmt;
+
+glib::wrapper! {
+    /// A GObject-derived object for sound unit of RME Fireface series.
+    ///
+    /// The [`SndFireface`][crate::SndFireface] is object class derived from `GObject::Object` for sound unit of
+    /// RME Fireface series supported by ALSA fireface driver (`snd-fireface`).
+    ///
+    /// # Implements
+    ///
+    /// [`AlsaFirewireExt`][trait@crate::prelude::AlsaFirewireExt], [`TimestampedQuadletNotificationExt`][trait@crate::prelude::TimestampedQuadletNotificationExt]
+    #[doc(alias = "HitakiSndFireface")]
+    pub struct SndFireface(Object<ffi::HitakiSndFireface, ffi::HitakiSndFirefaceClass>) @implements AlsaFirewire, TimestampedQuadletNotification;
+
+    match fn {
+        type_ => || ffi::hitaki_snd_fireface_get_type(),
+    }
+}
+
+impl SndFireface {
+    pub const NONE: Option<&'static SndFireface> = None;
+
+    /// Instantiate [`SndFireface`][crate::SndFireface] object and return the instance.
+    ///
+    /// # Returns
+    ///
+    /// an instance of [`SndFireface`][crate::SndFireface].
+    #[doc(alias = "hitaki_snd_fireface_new")]
+    pub fn new() -> SndFireface {
+        unsafe { from_glib_full(ffi::hitaki_snd_fireface_new()) }
+    }
+}
+
+impl Default for SndFireface {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl fmt::Display for SndFireface {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("SndFireface")
+    }
+}

commit 539b808bb9b4389251620142e5f21bd967eb5cd4

hitaki: format subclass according to Rust convention
Fixes: db5e859ea7da ("hitaki: regenerate crate for libhitaki v0.3.0 release")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

hitaki/src/subclass/timestamped_quadlet_notification.rs
@@ -90,24 +90,26 @@ mod test {
             fn properties() -> &'static [ParamSpec] {
                 use once_cell::sync::Lazy;
                 static PROPERTIES: Lazy<Vec<ParamSpec>> = Lazy::new(|| {
-                    vec![ParamSpecUInt::new(
-                        "msg",
-                        "msg",
-                        "the message in notification",
-                        0,
-                        0x0000ffff as u32,
-                        0,
-                        ParamFlags::READABLE,
-                    ),
-                    ParamSpecUInt::new(
-                        "tstamp",
-                        "tstamp",
-                        "the time stamp in notification",
-                        0,
-                        0x0000ffff as u32,
-                        0,
-                        ParamFlags::READABLE,
-                    )]
+                    vec![
+                        ParamSpecUInt::new(
+                            "msg",
+                            "msg",
+                            "the message in notification",
+                            0,
+                            0x0000ffff as u32,
+                            0,
+                            ParamFlags::READABLE,
+                        ),
+                        ParamSpecUInt::new(
+                            "tstamp",
+                            "tstamp",
+                            "the time stamp in notification",
+                            0,
+                            0x0000ffff as u32,
+                            0,
+                            ParamFlags::READABLE,
+                        ),
+                    ]
                 });
 
                 PROPERTIES.as_ref()